038b6d4ad16e2a86a5157bc3946a532d4ed9168d
Hheinrich2000 <hodemniac@yahoo.com>
2026-02-08 15:20:15 +0000

    Update deploy (set base)

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/encargado.github.io/",
   server: {
     host: "::",
     port: 8080,

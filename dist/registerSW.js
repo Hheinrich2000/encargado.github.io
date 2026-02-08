7e836d0355f35cae155f1c74107715cecf58e995
Hheinrich2000 <hodemniac@yahoo.com>
2026-02-08 15:26:22 +0000

    Deploy: rebuild with base / (user site)

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/encargado.github.io/sw.js', { scope: '/encargado.github.io/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
